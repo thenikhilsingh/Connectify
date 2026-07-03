ac4a206237d36ec6e84f91f9c4498d3199c0cfbf
thenikhilsingh <nikhilsingh26042004@gmail.com>
2026-07-03 18:02:45 +0530

    created post and comment model

---
+++ backend/models/post.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    caption: {
+      type: String,
+      required: false,
+    },
+    file: {
+      url: {
+        type: String,
+        default: "",
+      },
+      public_id: {
+        type: String,
+        default: "",
+      },
+      originalName: {
+        type: String,
+        default: "",
+      },
+      type: {
+        type: String,
+        default: "",
+      },
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Post", postSchema);
